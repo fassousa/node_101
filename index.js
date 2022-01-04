16be5aac472d71511b9cddb1b1659bf9418c70f8
Fagnner Sousa <fagnnnersousa@yahoo.com.br>
2022-01-04 19:40:27 -0300

    Create event emitter

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+// Create custom event emitter
+
+const { EventEmitter } = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('lunch', () =>  {
+    console.log('tasty meal')
+})
+
+// Call event twice
+eventEmitter.emit('lunch');
+eventEmitter.emit('lunch');
