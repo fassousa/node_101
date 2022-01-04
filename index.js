c77d97b49e7817e0490ed948ef04698049fc7bb4
Fagnner Sousa <fagnnnersousa@yahoo.com.br>
2022-01-04 20:47:13 -0300

    Refactor using promises

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const { readFile } = require('fs');
+const { readFile } = require('fs').promises;
 
 const app = express();
 
-app.get('/', (request, response) => {
-    readFile('./home.html', 'utf8', (err, html) => {
-        if (err) {
-            response.status(500).send('Sorry, we have a problem.')
-        }
-        response.send(html);
-    });
+app.get('/', async (request, response) => {
+    response.send( await readFile('./home.html', 'utf8') );
 });
 
 app.listen(process.env.PORT || 3000, () => console.log(`Available on http://localhost:3000`));
